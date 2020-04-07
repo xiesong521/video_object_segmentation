8aa0240f6f4678736fb778c4632705b4fd8f3d67
xiesong521 <284505750@qq.com>
2020-04-07 23:03:08 +0800

    add

---
+++ xiesong_add.py
@@ -0,0 +1,12 @@
+#!/user/bin/python3
+#coding:utf-8
+"""
+    Author:XieSong
+    Email:18406508513@163.com
+    
+    Copyright:XieSong
+    Licence:MIT
+
+"""
+if __name__ == '__main__':
+    print('hello')
